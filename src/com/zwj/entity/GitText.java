f1460100c511967f9aac29fbb2a005fb0e9154fb
zwj <1628725633@qq.com>
2021-08-25 17:54:43 +0800

    zwj commit version1.1

---
+++ src/com/zwj/entity/GitText.java
@@ -0,0 +1,12 @@
+package com.zwj.entity;
+
+/**
+ * @author zwj
+ * @date 2021/8/25 - 17:50
+ */
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+        System.out.println("version1.1");
+    }
+}
